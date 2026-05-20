7ea8dc363f07826eee060e54a20340b460523ccd
ChihChengLiang <chihchengliang@gmail.com>
2026-05-20 13:52:50 +0800

    rollback dist feature

--- present.sh
+++ present.sh
@@ -2,10 +2,6 @@
 files=($(find notes -name "slides.md"))
 PS3="Choose a slide to present: "
 select f in "${files[@]}"; do
-  if [ -n "$f" ]; then
-    out="dist/${f%.md}.html"
-    mkdir -p "$(dirname "$out")"
-    marp --html --allow-local-files --output "$out" --preview "$f"
-  fi
+  [ -n "$f" ] && marp --allow-local-files --preview "$f"
   break
 done
